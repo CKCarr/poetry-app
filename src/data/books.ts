f57d642df7106d14297afa5974f9ce51a4a54c8e
CKCarr <119520583+CKCarr@users.noreply.github.com>
2025-02-25 21:34:58 -0600

    upload mock book data for testing

---
+++ src/data/books.ts
@@ -0,0 +1,50 @@
+// Mock data for books of bookshelf
+export interface BookType {
+  title: string;
+  color: string;
+  author?: string;
+  content: string;
+  image: string;
+}
+
+const books = [
+  {
+    author: "CKCarr",
+    title: "Poem Collection 1",
+    color: "#702632",
+    content:
+      "Ocean of chaos and contemplation,\n Sailing to the destination,\n wave after wave,\n of hopeless degradation,\n Nothing left but planks and pulleys,\n canvas spread as eager as a birds wings,\n wrapped up in the breeze,\n All of the crashing,\n and weather that brews,\n cannot tame the storm,\n of captain and crews,\n encompass a journey,\n through the wheel,\n and lens scope",
+    image: "/images/beachglass.jpg",
+  }, // Gunmetal
+  { title: "Poem Collection 2", color: "#362023" }, // Ultra Violet
+  { title: "Dreams & Verses", color: "#273469" }, // Delft Blue
+  { title: "Midnight Thoughts", color: "#1E2749" }, // Space Cadet
+  { title: "Echoes of the Heart", color: "#084C61" }, // Wenge
+  { title: "Lost in Words", color: "#6C698D" }, // Powder Blue
+  { title: "Poem Collection 1", color: "#30343F" }, // Gunmetal
+  { title: "Poem Collection 2", color: "#40798C" },
+  { title: "Dreams & Verses", color: "#273469" }, // Delft Blue
+  { title: "Midnight Thoughts", color: "#1E2749" }, // Space Cadet
+  { title: "Echoes of the Heart", color: "#705D56" }, // Wenge
+  { title: "Lost in Words", color: "#6C698D" },
+  { title: "Poem Collection 1", color: "#30343F" }, // Gunmetal
+  { title: "Poem Collection 2", color: "#104547" },
+  { title: "Dreams & Verses", color: "#273469" }, // Delft Blue
+  { title: "Midnight Thoughts", color: "#1E2749" }, // Space Cadet
+  { title: "Echoes of the Heart", color: "#705D56" }, // Wenge
+  { title: "Lost in Words", color: "#6C698D" },
+  { title: "Poem Collection 1", color: "#30343F" }, // Gunmetal
+  { title: "Poem Collection 2", color: "#40798C" },
+  { title: "Dreams & Verses", color: "#273469" }, // Delft Blue
+  { title: "Midnight Thoughts", color: "#1E2749" }, // Space Cadet
+  { title: "Echoes of the Heart", color: "#705D56" }, // Wenge
+  { title: "Lost in Words", color: "#6C698D" },
+  { title: "Poem Collection 1", color: "#30343F" }, // Gunmetal
+  { title: "Poem Collection 2", color: "#40798C" },
+  { title: "Dreams & Verses", color: "#273469" }, // Delft Blue
+  { title: "Midnight Thoughts", color: "#1E2749" }, // Space Cadet
+  { title: "Echoes of the Heart", color: "#705D56" }, // Wenge
+  { title: "Lost in Words", color: "#6C698D" },
+];
+
+export default books;
