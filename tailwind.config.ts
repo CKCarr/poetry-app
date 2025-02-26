e8d4a5f07da27d125c9a4ac60713609f4837d600
CKCarr <119520583+CKCarr@users.noreply.github.com>
2025-02-25 21:30:52 -0600

    Tailwind styles addition & updatied config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,34 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+const config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/styles/**/*.{css,scss}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        gunmetal: "#30343F",
+        "ghost-white": "#FAFAFF",
+        periwinkle: "#E4D9FF",
+        "delft-blue": "#273469",
+        "space-cadet": "#1E2749",
+        wenge: "#705D56",
+        "cool-gray": "#9097C0",
+        "powder-blue": "#A7BBEC",
+        "anti-flash-white": "#EEEEEE",
+        bone: "#ECE2D0",
+        "ultra-violet": "#6C698D",
+        timberwolf: "#D4D2D5",
+        silver: "#BFAFA6",
+        beaver: "#AA968A",
+        "dim-gray": "#6E6A6F",
+        cerulean: "#40798C",
+        "Midnight green": "#104547",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
