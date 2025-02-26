466e3e0ea352d102ed4a14d119da95213a8360fc
CKCarr <119520583+CKCarr@users.noreply.github.com>
2025-02-25 22:18:24 -0600

    update page for build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <Hero />
       <main className="flex-grow flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {/* <Colors /> */}
         <Bookshelf />
       </main>
       <Footer />
