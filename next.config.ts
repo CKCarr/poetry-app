38e711809ead17b49aec98c245f2cb0667a444b8
CKCarrillo <119520583+CKCarr@users.noreply.github.com>
2026-09-24 01:42:28 +0000

    updates to configs dev vs prod

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
-  basePath: "/poetry-app",
+  basePath: isProduction ? "/poetry-app" : "",
   images: { unoptimized: true },
 };
 
